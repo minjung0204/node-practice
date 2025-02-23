e1cc71d038683a45c6d194913d6f71e5ffe6ea45
Minjung Kim <fgh2018@naver.com>
2025-02-23 13:36:06 +0900

    AWS RDS 연동 & dotenv로 환경변수 등록

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@
 // 모듈
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-
-const PORT = 3000;
 
 // 라우팅
 const home = require("./src/routes/home"); 
@@ -24,32 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/", home); // use는 미들 웨어를 등록해주는 메서드
 
 
-
-// app.get("/", (req, res) => {
-//     res.render("home/index.ejs"); // 6번에 상위폴더를 지정하여 views 파일을 안 해도 된다.
-// });
-
-// app.get("/login", (req, res) =>{
-//     res.render("home/login.ejs"); // 6번에 상위폴더를 지정하여 views 파일을 안 해도 된다.
-// });
-
 module.exports = app;
 
-
-// // http는 내장 모듈
-// const http = require("http");
-// const app2 = http.createServer((req, res) => {
-//     res.writeHead(200, {"content-type": "text/html; charset=utf-8"});
-//     console.log(req.url);
-//     if(req.url === "/"){
-//         res.end("여기는 루트입니다.");
-//     }else if(req.url === "/login"){
-//         res.end("여기는 로그인 화면입니다."); 
-//     }
-// });
-
-// app2.listen(3001, () => {
-//     console.log("http로 가동된 서버입니다.");
-// });
 
 
